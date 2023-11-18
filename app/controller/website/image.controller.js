fc2048c05db61df281ea37e720f9fb4dcca92e13
Devika Wscube <devikamali87960@gmail.com>
2023-11-18 23:37:10 +0530

    'someChanges'

--- app/controller/website/image.controller.js
+++ app/controller/website/image.controller.js
@@ -7,14 +7,15 @@ module.exports.DisplayImage = async (req, res) => {
 module.exports.SaveImages = async (req, res) => {
 
     try {
-        // console.log(req.files);
-        res.send(req.files)
+
+        //for two different images..............
+
         // const { image, coverimage } = req.files;
         // const imageData = {
-            // image: image[0].filename,
-            // coverimage: coverimage[0].filename
-            // singleimage : req.file.filename (for single image)
-            
+        //   image: image[0].filename,
+        //   coverimage: coverimage[0].filename
+        //   singleimage : req.file.filename (for single image)
+
         // }
         // await Images.create(imageData);
 
@@ -24,13 +25,38 @@ module.exports.SaveImages = async (req, res) => {
         //     Data : imageData
         // }
         // res.send(response);
+
+        //for multiple images 
+
+        const image = [];
+        const coverimage = [];
+
+        (req.files).filter((v, i) => {
+            if (v.fieldname == 'image') {
+                image.push(v.filename);
+            } else if (v.fieldname == 'coverimage') {
+                coverimage.push(v.filename);
+            }
+        })
+        const imageData = {
+            image : JSON.stringify(image),
+            coverimage : JSON.stringify(coverimage)
+        }
+         await Images.create(imageData);
+
+        let response = {
+            status: 200,
+            Message: 'images Inserted Successfully.....',
+            Data : imageData
+        }
+        res.send(response);
     }
     catch {
-        // response = {
-        //     status: 200,
-        //     Message: 'error while Inserting images.....'
-        // }
-        // res.send(response); 
-        res.send('error....')
+        response = {
+            status: 200,
+            Message: 'error while Inserting images.....',
+        }
+        res.send(response); 
+        // res.send('error....')
     }
 } 
