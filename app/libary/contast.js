4aa7f33f7545ad4b5581b9144050efef4f4442ff
Devika Wscube <devikamali87960@gmail.com>
2023-11-19 22:21:05 +0530

    'someChanges'

---
+++ app/libary/contast.js
@@ -0,0 +1,16 @@
+define('status', {
+    active: {
+        value: 1,
+        caption: "Active"
+    },
+    inactive: {
+        value: 0,
+        caption: 'InActive'
+    }
+});
+
+define('variables', {
+    ACTIVE : 'Active',
+    INACTIVE : 'Inactive',
+    serverKey : '123456789'
+})
